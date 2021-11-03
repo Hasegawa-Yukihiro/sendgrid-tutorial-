80844ba00f04460cfccefd6d227ebb31d21dc175
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2021-11-03 19:26:10 +0900

    first commit

---
+++ webpack/webpack.config.base.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+//  aliasの読み込み
+const { Alias } = require("../config/scripts/alias.js");
+const alias = new Alias();
+
+module.exports = {
+  /** entry point */
+  entry: path.resolve(__dirname, "../src/index.tsx"),
+  /** output point */
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  /** https://qiita.com/YoshinoriKanno/items/322ae6e53daa35059c15 */
+  devtool: "eval-source-map",
+  /** jsファイルへの変換処理  */
+  module: {
+    rules: [
+      /** ts , tsx ファイルは ts-loader を使ってJSに変換 */
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: { transpileOnly: true } //  ビルドの高速化
+          }
+        ]
+      }
+    ]
+  },
+
+  /**
+   * webpack には import する際に、指定されたモジュールを検索して該当するファイルを探す仕組み（モジュール解決  ）がある
+   * resolve オプションはモジュール解決（モジュールの import を解決する仕組み）の設定を変更します。
+   */
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: alias.toWebpack()
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html"
+    })
+  ]
+};
---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,21 @@
+const baseConfig = require("./webpack.config.base");
+const { merge } = require("webpack-merge");
+
+/**
+ * @description 環境変数の設定
+ * @param DOMAIN ドメイン
+ * @param NODE_ENV 環境名
+ */
+
+const config = merge(baseConfig, {
+  mode: "development",
+  devServer: {
+    static: "dist",
+    open: true,
+    port: 3000,
+    /** 404のfallback先を /index.html にする */
+    historyApiFallback: true
+  }
+});
+
+module.exports = config;
