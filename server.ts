5c2d62f7717180ca5ca45db4e0e9a2729c846803
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2021-11-06 11:24:14 +0900

    express追加

--- server.ts
+++ server.ts
@@ -1,12 +1,14 @@
-const express = require("express");
+import express from "express";
+import rootDir from "app-root-path";
+
 const app = express();
 const path = require("path");
 
-app.use(express.static("dist"));
+app.use("/static", express.static(rootDir + "/dist"));
 //ここでbuildフォルダをパブリックフォルダに指定する
 app.get("*", (req, res) => {
   //buildフォルダ内のindex.htmlをリクエストに対して返信
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(rootDir + "/dist/index.html");
 });
 const port = process.env.PORT || 5000;
 
