85e49261403255bb7bd68d75754cb6321b4d9281
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2021-11-14 16:18:08 +0900

    CreateContact - apiの繋ぎ込み実装

--- client/src/api/Contact/CreateContact/index.ts
+++ client/src/api/Contact/CreateContact/index.ts
@@ -1,1 +1,25 @@
-export class CreateContact {}
+import { APIBase } from "~src/api/APIBase";
+import {
+  CreateContactAPIImplements,
+  CreateContactRequest,
+  CreateContactRequestBody,
+  CreateContactResponse
+} from "./type";
+
+export class CreateContact
+  extends APIBase
+  implements CreateContactAPIImplements
+{
+  constructor() {
+    super("SENDGRID_TUTORIAL_URL");
+  }
+
+  async handle(request: CreateContactRequest): Promise<CreateContactResponse> {
+    const response = await this.post<
+      CreateContactRequestBody,
+      CreateContactResponse
+    >("/contact", request.body);
+
+    return response.data;
+  }
+}
