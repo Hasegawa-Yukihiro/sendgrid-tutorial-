bc02b67773c8c0b94caee490afa7f652c2f1aa7f
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2021-11-14 15:12:27 +0900

    問い合わせフォームUI実装

--- client/src/Views/Pages/Contact/index.tsx
+++ client/src/Views/Pages/Contact/index.tsx
@@ -1,5 +1,44 @@
 import React from "react";
 
 export const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div
+      style={{
+        margin: "0 auto",
+        width: "50%",
+        display: "flex",
+        flexDirection: "column"
+      }}
+    >
+      <p>お名前</p>
+      <input
+        style={{
+          margin: "10 0",
+          height: 30,
+          padding: 5
+        }}
+        type="text"
+        name="name"
+      />
+      <p>メールアドレス</p>
+      <input
+        style={{
+          margin: "10 0",
+          height: 30,
+          padding: 5
+        }}
+        type="email"
+        name="email"
+      />
+      <p>お問い合せ内容</p>
+      <textarea
+        style={{
+          margin: "10 0",
+          height: 100,
+          padding: 5
+        }}
+        name="message"
+      />
+    </div>
+  );
 };
