d209397eea2d91cbae7870271d997c7a47f7c8b6
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2021-11-14 16:12:07 +0900

    apiBaseの追加

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import { Router } from "./Providers/Router";
+import { Router } from "./views/providers/Router";
 
 ReactDOM.render(<Router />, document.getElementById("root"));
