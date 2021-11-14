03010ee44c34664d68cd831058a83b56a36b9092
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2021-11-14 14:42:25 +0900

    senderとreciverのテンプレ作成

--- server/src/repository/Contact/ContactRepository.ts
+++ server/src/repository/Contact/ContactRepository.ts
@@ -6,7 +6,8 @@ export class ContactRepository implements ContactRepositoryProps.Impl {
   async create(contact: Contact): Promise<boolean> {
     sgMail.setApiKey(process.env.SENDGRID_API_KEY || "");
 
-    const msg = {
+    /** 送信者へ送るメール */
+    const senderMsg = {
       to: contact.email.value,
       from: "y.h.baskeeee@icloud.com",
       subject: "Sending with SendGrid is Fun",
@@ -15,7 +16,25 @@ export class ContactRepository implements ContactRepositoryProps.Impl {
     };
 
     sgMail
-      .send(msg)
+      .send(senderMsg)
+      .then(val => {
+        console.log("success", val);
+      })
+      .catch(err => {
+        console.log("failed", err);
+      });
+
+    /** 受信者（管理者）へ送るメール */
+    const receiverMsg = {
+      to: "y.h.baskeeee@icloud.com",
+      from: "y.h.baskeeee@icloud.com",
+      subject: "以下の問い合わせがありました。",
+      text: "以下の問い合わせがありました。",
+      html: `<p>${contact.message.value}</p>`
+    };
+
+    sgMail
+      .send(receiverMsg)
       .then(val => {
         console.log("success", val);
       })
