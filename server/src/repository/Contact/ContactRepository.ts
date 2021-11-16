b0835dc78848ac5fc5e8a4119a682d906b7662d1
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2021-11-16 19:55:13 +0900

    lin - sendGrid実装

--- server/src/repository/Contact/ContactRepository.ts
+++ server/src/repository/Contact/ContactRepository.ts
@@ -1,52 +1,20 @@
-import sgMail from "@sendgrid/mail";
 import { Contact } from "src/domain/Models/Contact";
+import { sendGrid, SendGridOption } from "~src/lib/SendGrid";
 import { ContactRepositoryProps } from "./type";
 
 export class ContactRepository implements ContactRepositoryProps.Impl {
   async create(contact: Contact): Promise<boolean> {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY || "");
-
-    /** 送信者へ送るメール */
-    const senderMsg = {
-      to: contact.email.value,
-      from: {
-        email: "y.h.baskeeee@icloud.com",
-        name: "長谷川 有希紘"
-      },
-      subject: `${contact.name.value}様 お問い合わせありがとうございます。`,
-      text: "以下の問い合わせを受け付けました。",
-      html: `<p>以下の問い合わせを受け付けました。<br>${contact.message.value}</p>`
+    const user: SendGridOption.User = {
+      name: contact.name.value,
+      email: contact.email.value,
+      message: contact.message.value
     };
 
-    sgMail
-      .send(senderMsg)
-      .then(val => {
-        console.log("success", val);
-      })
-      .catch(err => {
-        console.log("failed", err);
-      });
-
-    /** 受信者（管理者）へ送るメール */
-    const receiverMsg = {
-      to: "y.h.baskeeee@icloud.com",
-      from: {
-        email: "y.h.baskeeee@icloud.com",
-        name: "長谷川 有希紘"
-      },
-      subject: "以下の問い合わせがありました。",
-      text: "以下の問い合わせがありました。",
-      html: `<p>${contact.name.value}様<br>${contact.message.value}</p>`
-    };
-
-    sgMail
-      .send(receiverMsg)
-      .then(val => {
-        console.log("success", val);
-      })
-      .catch(err => {
-        console.log("failed", err);
-      });
+    try {
+      await sendGrid.contactEmail(user);
+    } catch (e) {
+      console.log("failed", e);
+    }
 
     return true;
   }
